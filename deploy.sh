1ef28bf1ac1a745d23ea5dc4aef5a8c699150cd6
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-16 19:10:52 +0530

    clean up default template

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+
+#clean up public
+echo -e "\033[0;32mDeleting old copy...\033[0m"
+rm -rf deployment/*
+
+# Build the project.
+npm run build
+
+# Go To Public folder
+cd deployment
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding portfolio `date`"
+if [ $# -eq 1 ]
+then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
