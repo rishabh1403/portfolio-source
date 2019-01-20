14e9be970225f211592fa3419fadc59fdfd111f5
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-20 06:34:38 +0530

    deployed portfolio on Sun Jan 20 06:34:38 IST 2019

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Message from './Message';
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      command: "",
+      commands: [],
+    }
+  }
+
+  handleOnChange(e) {
+    this.setState({
+      command: e.target.value
+    })
+  }
+  renderCommands() {
+    return this.state.commands.map(el => {
+      return <Message command={el} />
+    })
+  }
+  handleClick() {
+    this.setState({
+      commands: [...this.state.commands, this.state.command]
+    }, () => {
+      this.setState({
+        command: ""
+      })
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-
-          Learn React
-        </header>
+        {this.renderCommands()}
+        <div>
+          <div>Type here <span><input value={this.state.command} onChange={this.handleOnChange.bind(this)} /></span></div>
+          <button onClick={this.handleClick.bind(this)} >Add Command</button>
+        </div>
       </div>
     );
   }
---
+++ src/Message.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Message extends Component {
+
+ 
+
+  render() {
+    return (
+      <div>
+        {this.props.command}
+      </div>
+    );
+  }
+}
+
+export default Message;
