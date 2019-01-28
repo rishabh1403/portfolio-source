1010d16121648dd2183b7843d5b2104e18f0ee72
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-28 06:12:12 +0530

    add help text

--- src/WelcomeText.js
+++ src/WelcomeText.js
@@ -4,7 +4,8 @@ export default class extends Component {
   render() {
     return (
       <React.Fragment>
-        <div>Type <code>help</code> in case you need help navigating through the page</div>
+        <div>Type <code>ls</code> to list contents of directory</div>
+        <div>Type <code>cd {`directory name`}</code> to change directory</div>
       </React.Fragment>
     );
   }
