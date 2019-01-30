efc6cbe44e06741322c831fab97dab7de9b53636
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-30 23:13:17 +0530

    add error component

--- src/Error.js
+++ src/Error.js
@@ -4,7 +4,10 @@ import PropTypes from 'prop-types';
 const Error = ({ data }) => {
   if (data.code === 'NOT_A_FILE') {
     return (
-      <div className="error-msg">This cannot be a dat {data.data}</div>
+      <div className="error-msg">
+        This cannot be a dat
+        {data.data}
+      </div>
     );
   }
   return null;
