e2f9eb4599a911c26df2ae04c2bfbcd9d0ffb195
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-01 07:48:56 +0530

    clean ls and cat

--- src/__tests__/commands/pwd.test.js
+++ src/__tests__/commands/pwd.test.js
@@ -1,32 +1,31 @@
 import { pwd } from '../../commands';
 
 describe('Testing pwd command', () => {
+  // it.each([
+  //   [[], { success: true }],
+  //   [[], { success: true }],
+  //   [[], { success: true }],
+  //   [[], { success: true }]])('Testing it', (a, ex) => {
+  //     expect(pwd(a)).toBeTruthy();
+  //   });
 
-  it.each([
-    [[], { success: true }],
-    [[], { success: true }],
-    [[], { success: true }],
-    [[], { success: true }]])('Testing it', (a, ex) => {
-      expect(pwd(a)).toBeTruthy();
-    });
-
-  // it('should be defined', () => {
-  //   expect(pwd).toBeDefined();
-  // });
-  // it('should return an object with success true', () => {
-  //   const path = [];
-  //   expect(pwd(path).success).toBeTruthy();
-  // });
-  // it('should return an object with type pwd', () => {
-  //   const path = [];
-  //   expect(pwd(path).type).toBe('PWD');
-  // });
-  // it('should return ~/ for root path', () => {
-  //   const path = [];
-  //   expect(pwd(path).data).toBe('~/');
-  // });
-  // it('should join path array to give complete path', () => {
-  //   const path = ['about'];
-  //   expect(pwd(path).data).toBe('~/about');
-  // });
+  it('should be defined', () => {
+    expect(pwd).toBeDefined();
+  });
+  it('should return an object with success true', () => {
+    const path = [];
+    expect(pwd(path).success).toBeTruthy();
+  });
+  it('should return an object with type pwd', () => {
+    const path = [];
+    expect(pwd(path).type).toBe('PWD');
+  });
+  it('should return ~/ for root path', () => {
+    const path = [];
+    expect(pwd(path).data).toBe('~/');
+  });
+  it('should join path array to give complete path', () => {
+    const path = ['about'];
+    expect(pwd(path).data).toBe('~/about');
+  });
 });
